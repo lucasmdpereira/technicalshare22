bc26655018d5a5e8276fd78a105c3843da524fc0
lucasmdpereira <lucasmdpereira@gmail.com>
2022-04-04 15:46:54 -0300

    tagfilter finish

--- public/scripts/perfil.js
+++ public/scripts/perfil.js
@@ -5,7 +5,7 @@ searchButton.addEventListener("click", formSearch)
 function formSearch(){
     let userName = document.querySelector('#nameUser').innerHTML
     let userSearch = document.querySelector('#user_search').value
-    console.log(userSearch)
+    //console.log(userSearch)
     let form = document.querySelector('#search_form')
     
     form.setAttribute('action', `/search/${userName}/${userSearch}`)
--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -6,23 +6,32 @@ module.exports = {
         let sql
 
         let search = req.params.userSearch.toLowerCase()
-        console.log('search= ' +  search)
-        console.log('results: ')
+        // console.log('search= ' +  search)
+        // console.log('results: ')
 
         const db = new sqlite3.Database(path.join(__dirname, '..', 'db', 'users.db'), sqlite3.OPEN_READWRITE, (err) => {
             if (err) return console.error(err.message)
         } )
 
-        regExrsearch = `%#%${search}%#%`
+        regExrsearch = `%#%${search}%¢%`
         sql = `SELECT ALL * FROM users WHERE tags LIKE '${regExrsearch}'`
         
         db.all(sql,[],(err,row)=>{
             let userName = req.params.userName
             if (err) return console.error(err.message)
             if (row == 0) return console.error("not found")
-            //res.status(404).json({erro: 'Usuário não encontrado, pro favor contacte o administrador do sistema'})
-            res.render('search', {user: userName, data: row, search: true})
+            //res.status(404).json({erro: 'Usuário não encontrado, pro favor contacte o administrador do sistema'})     
+            for (let i = 0; i < row.length; i++){
+                row[i].tags = splitTags(row[i].tags)
+            }
             console.log(row)
+            
+            res.render('search', {user: userName, data: row, search: true, number_of_results: row.length})
+            //console.log(row)
         })
     }
+}
+
+function splitTags(tags){
+    return tags.split('¢').filter(Boolean);
 }
