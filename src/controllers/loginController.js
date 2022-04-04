0766aaacd82b08cfa04035bca3fa8bdc14e0585f
lucasmdpereira <lucasmdpereira@gmail.com>
2022-04-04 19:38:07 -0300

    login && perfil bootstraped

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -23,18 +23,24 @@ module.exports = {
         db.all(sql,[],(err,row)=>{
         if (err) return console.error(err.message)
         if (row == 0) return res.status(404).json({erro: 'Usuário não encontrado, pro favor contacte o administrador do sistema'}) //aparecer pro usuário
-
+        for (let i = 0; i < row.length; i++){
+            row[i].tags = splitTags(row[i].tags)
+        }
         if (userPassword == row[0].password){
             console.log('Login válido')
             //res.use(`/perfil/${row[0].name}`, redirect)
             //res.redirect(300, '/perfil')
-            res.render('perfil', {user: row[0].name, data: ''})
+            res.render('perfil', {user: row[0]})
                 
         } else {
             console.error('Login inválido')
         }
 
 })
+
+function splitTags(tags){
+    return tags.split('¢').filter(Boolean);
+}
 
      }
  }
