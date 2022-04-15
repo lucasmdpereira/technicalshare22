940fee7a22e150d68c8ca835f5592e69ed491123
lucasmdpereira <lucasmdpereira@gmail.com>
2022-04-15 08:47:07 -0300

    last day organization

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -4,9 +4,8 @@ sendButton.addEventListener("click", sendFormLogin)
 function sendFormLogin() {
     let userCredentials = document.querySelector('#floatingInput').value
 
-    let userPassword = document.querySelector('#floatingPassword').value
-
     const form = document.querySelector('#credentials_form')
+    console.log(form)
     form.setAttribute('action', `/home/${userCredentials}`)
     form.setAttribute('method', 'POST')
 }
